6f9abb02aecc35a41d682f86601bef088b2ea870
Eyimofe Ogunbiyi <ogunbiyioladapo33@gmail.com>
2021-09-09 04:27:30 +0100

    complete

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    private float speed = 20.0f;
+    [SerializeField]
+    private float turnSpeed = 45.0f;
+    [SerializeField]
+    private float horizontalInput;
+    [SerializeField]
+    private float forwardInput;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+    }
+}
